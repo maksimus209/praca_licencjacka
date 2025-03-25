c975fdfc30e30ba00ccfb18498a8d9890e80c840
Maksym <makssshvets@gmail.com>
2025-03-25 22:01:58 +0100

    przykładowa porównywarka połączona z phpMyadmin

--- autoryzacja.php
+++ autoryzacja.php
@@ -1,8 +1,11 @@
 <?php
-
     $dbhost='localhost';
     $dbuser='22_shvets';
     $dbpass='132435maxik';
     $dbname='22_shvets';
 
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($conn->connect_error) {
+    die("Połączenie nieudane: " . $conn->connect_error);
+}
 ?>
